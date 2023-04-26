6ad19374d0648059950fb55d6bb6acaad45ba82d
Viola Zhong <viola.zhong@doubleverify.com>
2023-04-26 14:23:31 -0400

    first commit

---
+++ EnigmaSimulator.Test/Components/PlugBoardTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EnigmaSimulator.Test.Components
+{
+	public class PlugBoardTest
+	{
+		public PlugBoardTest()
+		{
+		}
+	}
+}
+
---
+++ EnigmaSimulator.Test/ProgramTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EnigmaSimulator.Test
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+        }
+    }
+}
